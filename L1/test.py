2aa2c5b82d3d7220c94256987905ba505a30b1d0
Damir <18482283+damkh@users.noreply.github.com>
2021-12-14 18:48:54 +0300

    test.pe removed

--- L1/test.py
+++
@@ -1,5 +0,0 @@
-oct1, oct2, oct3, oct4 = [10, 1, 1, 1]
-
-print(f'''IP address: {oct1:<8} {oct2:<8} {oct3:<8} {oct4:<8}''')
-print(f'''IP address: {oct1:<2} {oct2:<8} {oct3:<8} {oct4:<8}''')
-print(f'''IP address: {oct1:<2} {oct2:<8} {oct3:<8} {oct4:<8}''')
