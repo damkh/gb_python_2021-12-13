b3771cbe781cec3b9ac56ab3761e9b936f185d68
Damir <18482283+damkh@users.noreply.github.com>
2021-12-14 15:50:13 +0300

    second commit

---
+++ L1/lesson_1_task_1.py
@@ -0,0 +1,18 @@
+a = 1
+b = 'some string'
+c = [1, 2, 3, 4]
+d = {
+    "a": 5,
+    "b": "qq",
+    "c": [5, 6, 7]
+}
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+a1 = int(input("Введите число: "))
+b1 = input("Введите строку: ")
+
+print(f"Строка: {b1}, Число: {a1}")
