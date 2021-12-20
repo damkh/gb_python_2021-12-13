8d2fa2e544d23167a5b8c49cee33f763473587df
Damir <18482283+damkh@users.noreply.github.com>
2021-12-20 21:52:51 +0300

    lesson_4_task_1.py

---
+++ L4/lesson_4_task_1.py
@@ -0,0 +1,33 @@
+# 1. Реализовать скрипт, в котором должна быть предусмотрена функция расчета заработной платы сотрудника. В расчете
+# необходимо использовать формулу: (выработка в часах*ставка в час) + премия. Для выполнения расчета для конкретных
+# значений необходимо запускать скрипт с параметрами.
+
+from sys import argv
+
+
+def calc_salary(f_work_hours, f_hourly_rate, f_bonus):
+    salary = int(f_work_hours) * int(f_hourly_rate) + int(f_bonus)
+    print(f'Salary: {salary}')
+
+
+# параметрами по умолчанию
+work_hours = 5
+hourly_rate = 2
+bonus = 3
+
+if len(argv) == 1:  # Вычисление с параметрами по умолчанию
+    print(f'Вычисление с параметрами по умолчанию: '
+          f'work hours = {work_hours}, hourly rate = {hourly_rate}, bonus = {bonus}')
+    calc_salary(work_hours, hourly_rate, bonus)
+if len(argv) == 2:
+    print('Недостаточно параметров')
+elif len(argv) == 3:  # Без премии
+    script_name, work_hours, hourly_rate = argv
+    calc_salary(work_hours, hourly_rate, 0)
+elif len(argv) == 4:  # С премией
+    script_name, work_hours, hourly_rate, bonus = argv
+    calc_salary(work_hours, hourly_rate, bonus)
+elif len(argv) > 4:
+    print('Есть лишние параметры')
+
+
